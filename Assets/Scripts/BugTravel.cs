becbe271a7e73de30eca85c3bba139b4562175b0
Geoffrey Ding <firstdreamding@gmail.com>
2025-05-27 15:03:24 -0700

    supports multiple ladybugs with wonky movements lol

--- Assets/Scripts/BugTravel.cs
+++ Assets/Scripts/BugTravel.cs
@@ -1,35 +1,66 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BugTravel : MonoBehaviour
 {
     // TODO: currently acts on a single bug.
     // need multiple bugs as well as types of bug.
-    public GameObject bug;
-    private Transform bugTransform;
+    // private GameObject bug;
+    // private Transform bugTransform;
 
-    public float speed;
-    public Vector3 travelOrigin;
-    public float travelRadius;
+    // public float speed;
+    // public Vector3 travelOrigin;
+    // public float travelRadius;
 
     private Vector3 dest;
+
+    public struct Metadata
+    {
+        public float speed;
+        public Vector3 origin;
+        public float radius;
+
+        public Metadata(float speed, Vector3 origin, float radius)
+        {
+            this.speed = speed;
+            this.origin = origin;
+            this.radius = radius;
+        }
+    }
+
+    private Dictionary<string, Metadata> metadata = new Dictionary<string, Metadata>
+    {
+        {"LadyBug", new Metadata(1.0f, new Vector3(0.0f, 0.0f, 0.0f), 5.0f) }
+    };
+
+    private GameObject[] bugs;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        bugTransform = bug.transform;
+        bugs = GameObject.FindGameObjectsWithTag("LadyBug");
+        // bugTransform = bug.transform;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Travel();
+        foreach (GameObject bug in bugs)
+        {
+            Metadata rangeData = metadata["LadyBug"];
+            Travel(bug.transform, rangeData);
+            Debug.DrawLine(bug.transform.position, dest, Color.blue);
+        }
+        // Travel();
         //Debug.DrawRay(dest, Vector3.up * 0.01f, Color.blue);
-        Debug.DrawLine(bugTransform.position, dest, Color.blue);
+        
     }
 
     void OnDrawGizmos()
     {
-        DrawCircle(travelOrigin, travelRadius, 32, Color.green);
+        Metadata rangeData = metadata["LadyBug"];
+        DrawCircle(rangeData.origin, rangeData.radius, 32, Color.green);
     }
 
     void DrawCircle(Vector3 center, float radius, int segments, Color color)
@@ -46,15 +77,15 @@ public class BugTravel : MonoBehaviour
         }
     }
 
-    void Travel()
+    void Travel(Transform bugTransform, Metadata rangeData)
     {
-        if (bug.transform.position == dest)
+        if (bugTransform.position == dest)
         {
             // TODO: this currently just moves the bug within a square lol.
             // Consider restricting movement to a circle? or a probability density fn would be fun.
-            dest = new Vector3(Random.Range(travelOrigin.x-travelRadius, travelOrigin.x+travelRadius),
+            dest = new Vector3(Random.Range(rangeData.origin.x-rangeData.radius, rangeData.origin.x+rangeData.radius),
                 0.0f,
-                Random.Range(travelOrigin.z-travelRadius, travelOrigin.z+travelRadius));
+                Random.Range(rangeData.origin.z-rangeData.radius, rangeData.origin.z+rangeData.radius));
             Debug.Log("new point drawn at " + dest);
             return;
         }
@@ -66,5 +97,5 @@ public class BugTravel : MonoBehaviour
         Quaternion rotation = Quaternion.LookRotation(bugToDest);
 
         bugTransform.rotation = Quaternion.Slerp(bugTransform.rotation, rotation, 0.1f);
-        bugTransform.position = Vector3.MoveTowards(bugTransform.position, dest, speed * Time.deltaTime);    }
+        bugTransform.position = Vector3.MoveTowards(bugTransform.position, dest, rangeData.speed * Time.deltaTime);    }
 }
