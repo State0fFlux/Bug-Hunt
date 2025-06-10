3c6307562996a8578c11e480f081691747949795
State0fFlux <bmanske505@gmail.com>
2025-06-10 05:16:50 -0700

    Added credits page to lobby menu

--- Assets/Scripts/MenuNavigator.cs
+++ Assets/Scripts/MenuNavigator.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 
 public class MenuNavigator : MonoBehaviour
 {
+    public GameObject popupPanel; // Reference to the popup panel GameObject
+
+    private void Start()
+    {
+        // Ensure the popup panel is inactive at the start
+        if (popupPanel != null)
+        {
+            popupPanel.SetActive(false);
+        }
+    }
+    
     public void Play()
     {
         // Load the game scene
@@ -19,14 +30,24 @@ public class MenuNavigator : MonoBehaviour
         yield return new WaitForSeconds(0.25f);
         Application.Quit();
         // If running in the editor, stop playing
-        #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 
     public void SkipCutscene()
     {
         // Load the game scene directly, skipping the cutscene
         UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenPopup()
+    {
+        popupPanel.SetActive(true); // Activate the popup panel
+    }
+
+    public void ClosePopup()
+    {
+        popupPanel.SetActive(false); // Deactivate the popup panel
     }
 }
